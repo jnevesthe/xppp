600feac2694513b8e3f6b1efef7abad3f25c4dc1
jnevesthe <nevesjosemar9@gmail.com>
2026-04-11 09:35:02 +0100

    Update urls.py

--- video/urls.py
+++ video/urls.py
@@ -1,6 +1,7 @@
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
